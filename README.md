10155be04c4c86daaf5cc206132e8795763ec3ea
Guoliang Li <guoliang.me@gmail.com>
2015-12-04 23:54:56 +0800

    init import

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# rabbit-youtube
+# README #
+
 A utility to download youtube videos by user_id / playlist_id.
+
+Please note you'll need to have a google API key (Browser key) to use this application, you may get a key from: https://console.developers.google.com
+
+### Main Feature ###
+
+* rabbit-youtube.py -p PLyPg0ySdyY2b4EbCZFdGUScU1xC3RjkXo ~/tmp-test
+* rabbit-youtube.py -u GoogleDevelopers ~/tmp-test
+* skip the downloaded files
---
+++ rabbit_youtube/tests/test_youtube_api.py
@@ -0,0 +1,62 @@
+import json
+import os
+import unittest
+from urllib2 import urlopen
+from pytube import YouTube
+from rabbit_youtube import app_config
+
+
+key = app_config.get_google_api_key()
+
+
+class TestYoutubeAPI(unittest.TestCase):
+    def test_get_channel_id_by_user_id(self):
+        user_id = 'GoogleDevelopers'
+        user_channel_url_base = 'https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=%s&key=%s'
+
+        response = urlopen(user_channel_url_base % (user_id, key))
+        content = response.read().encode('utf-8')
+        data = json.loads(content)
+
+        print 'channel id data: \n%s: ' % data
+
+        self.assertTrue(data is not None)
+
+    def test_get_video_id_list(self):
+        upload_playlist_id = 'UU_x5XG1OV2P6uZZ5FSM9Ttw'
+        upload_playlist_url_base = 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=%s&key=%s'
+        url_playlist = upload_playlist_url_base % (upload_playlist_id, key)
+
+        content = urlopen(url_playlist).read().decode('utf-8')
+        data = json.loads(content)
+
+        print 'Video Id List: \n%s' % data
+
+        self.assertTrue(data is not None)
+
+
+class TestPytube(unittest.TestCase):
+
+    def test_get_videos_by_video_id(self):
+        video_id = 'uFOWdNtUgvU'
+        video_info_url_base = 'https://www.youtube.com/watch?v=%s'
+        video_url = video_info_url_base % video_id
+        youtube = YouTube(video_url)
+        self.assertGreater(youtube.get_videos(), 1)
+
+    def test_download_video(self):
+        video_id = 'cr04UelmPqg'  # https://www.youtube.com/watch?v=cr04UelmPqg
+        video_info_url_base = 'https://www.youtube.com/watch?v=%s'
+        video_url = video_info_url_base % video_id
+        youtube = YouTube(video_url)
+
+        video = youtube.get_videos()[0]  # get_best_video(youtube)
+
+        file_path = '/tmp/rabbit_youtube_test.%s' % video.extension
+
+        video.download(file_path)
+        self.assertTrue(os.path.isfile(file_path))
+        os.remove(file_path)
+
+if __name__ == '__main__':
+    unittest.main()
